19b39147b8373fc162ee7d569a9c439bb90553a4
Damian Tabor <zettie008@gmail.com>
2021-08-11 23:30:43 +0100

    add: inline address to user model

--- address/admin.py
+++ address/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import User, Address
 
+
+#admin.site.register(User, UserAdmin)
+#admin.site.register(Address)
+
+
+class AddressInline(admin.TabularInline):
+    model = Address
+
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [
+        AddressInline,
+    ]
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Address)
