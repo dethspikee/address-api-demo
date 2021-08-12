8365541e3e56cf696846a91cde7d04f866319073
Damian Tabor <zettie008@gmail.com>
2021-08-12 21:55:57 +0100

    change: use standard urlpatterns

--- address/urls.py
+++ address/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter
 
 from .views import AddressView
 
 
-router = SimpleRouter()
-router.register('', AddressView, basename='addresses')
-
-
-urlpatterns = router.urls
+urlpatterns = [
+    path('', AddressView.as_view()),
+]
