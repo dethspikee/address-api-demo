dbad2f05207c1e21c9f0cdffecbbf09e52283779
Damian Tabor <zettie008@gmail.com>
2021-08-12 22:07:52 +0100

    add: browsable login and urls for dj-rest-auth package

--- address/urls.py
+++ address/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import AddressView
 
 
 urlpatterns = [
     path('', AddressView.as_view()),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
