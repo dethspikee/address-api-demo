6018fa616cded3a9d1ac76d6b19d47b81c579131
Damian Tabor <zettie008@gmail.com>
2021-08-17 20:55:11 +0100

    make sure blank values are not permitted & run migrations

--- address/models.py
+++ address/models.py
@@ -11,11 +11,14 @@ class User(AbstractUser):
 class Address(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, 
             related_name='address')
-    street = models.CharField(max_length=250)
-    postcode = models.CharField(max_length=30)
-    town = models.CharField(max_length=30)
-    country = models.CharField(max_length=30)
+    street = models.CharField(max_length=250, blank=False)
+    postcode = models.CharField(max_length=30, blank=False)
+    town = models.CharField(max_length=30, blank=False)
+    country = models.CharField(max_length=30, blank=False)
     current = models.BooleanField()
+
+    class Meta:
+        unique_together = ['street', 'postcode', 'town', 'country']
 
     def __str__(self):
         return f'Address for user: {self.user.first_name}'
