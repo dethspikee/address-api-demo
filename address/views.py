843d40156795695eede34282a5c9dd52c4779b33
Damian Tabor <zettie008@gmail.com>
2021-08-17 22:31:34 +0100

    handle POST view

--- address/views.py
+++ address/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -6,16 +7,32 @@ from .models import User
 
 
 class AddressView(APIView):
-
     def get(self, request, format=None):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
     
     def post(self, request):
+        user_uuid = request.data.get("user_uuid")
+        if not user_uuid:
+            error_message = {
+                "error": {
+                    "type": "Missing field.",
+                    "message": "user_uuid missing."
+                }
+            }
+            return Response(error_message, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(user_uuid=user_uuid)
+        except ValidationError as e:
+            return Response({"field_error": e}, status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist as e:
+            return Response({"field_error": "User doesn't exist"}, status=status.HTTP_400_BAD_REQUEST)
+
         serializer = AddressSerializer2(data=request.data)
         if serializer.is_valid():
-            serializer.save(user_id=2)
+            serializer.save(user_id=user.id)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors,
