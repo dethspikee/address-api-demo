81df8677621affcd042a5bbcb1afc4bbaca84ea3
Damian Tabor <zettie008@gmail.com>
2021-08-23 22:26:24 +0100

    Changed models and created migrations

--- address/models.py
+++ address/models.py
@@ -1,12 +1,9 @@
-import uuid
-
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    user_uuid = models.UUIDField(default=uuid.uuid4, editable=False)
-
+    pass
 
 class Address(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, 
