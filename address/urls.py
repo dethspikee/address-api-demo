ca0013f916ab1be1d629a5b7d1b17297a4beb6a8
Damian Tabor <zettie008@gmail.com>
2021-08-27 13:47:28 +0100

    Add extra url pattern for handling PATCH requests

--- address/urls.py
+++ address/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from .views import AddressView
+from .views import AddressView, AddressViewPatch
 
 
 urlpatterns = [
+    path('address/<int:pk>/', AddressViewPatch.as_view()),
     path('', AddressView.as_view()),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
