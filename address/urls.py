6b6242e39281568df17268e9b9043251d9fdd383
Damian Tabor <zettie008@gmail.com>
2021-08-30 18:54:38 +0100

    Update imports

--- address/urls.py
+++ address/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 
-from .views import AddressView, AddressViewPatch
+from .views import AddressView, AddressDetail
 
 
 urlpatterns = [
-    path('address/<int:pk>/', AddressViewPatch.as_view()),
+    path('address/<int:pk>/', AddressDetail.as_view()),
     path('', AddressView.as_view()),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
