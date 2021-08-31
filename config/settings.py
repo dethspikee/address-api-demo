f9b2e35e0a94c674c871cf4feed64f6202e19e59
Damian Tabor <zettie008@gmail.com>
2021-08-31 19:09:08 +0100

    Reorder classes

--- config/settings.py
+++ config/settings.py
@@ -137,8 +137,8 @@ AUTH_USER_MODEL = 'address.User'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
+    'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'NON_FIELD_ERRORS_KEY': 'error',
 }
