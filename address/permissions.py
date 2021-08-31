5d5a2e2e0ce44399090ba876dd77a4726a67e4e6
Damian Tabor <zettie008@gmail.com>
2021-08-31 18:57:31 +0100

    Make custom permission to make sure users can only see their addresses

--- address/permissions.py
+++ address/permissions.py
@@ -2,9 +2,6 @@ from rest_framework import permissions
 
 
 class OwnerOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return True
 
     def has_object_permission(self, request, view, obj):
-        print("Calling object permissions!")
-        return False
+        return request.user == obj.user
