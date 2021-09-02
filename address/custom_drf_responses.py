da1b7ad3f10426b5bf039d0ae5d9a7da44e5d322
Damian Tabor <zettie008@gmail.com>
2021-09-02 22:23:28 +0100

    Provide custome description and custom schema

--- address/custom_drf_responses.py
+++ address/custom_drf_responses.py
@@ -1,11 +1,13 @@
 from rest_framework.response import Response
+from .serializers import MyTokenSerializer
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
 
 register_response = {
     "201": openapi.Response(
-        description="test",
+        schema=MyTokenSerializer,
+        description="",
         examples={
             "application/json": {"username": "string"}
         }
