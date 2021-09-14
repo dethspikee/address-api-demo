aaba7c7890dadeaa04196345f8b91c21c968393e
Damian Tabor <zettie008@gmail.com>
2021-09-14 19:18:40 +0100

    Change to Class based test

--- tests/address/test_models.py
+++ tests/address/test_models.py
@@ -4,14 +4,16 @@ from django.db import IntegrityError
 from address.models import Address, User
 
 
-@pytest.mark.django_db
-def test_address_model_raises_integrityerror():
-    with pytest.raises(IntegrityError):
-        Address.objects.create()
+class TestAddressModel:
+
+    @pytest.mark.django_db
+    def test_address_model_raises_integrityerror(self):
+        with pytest.raises(IntegrityError):
+            Address.objects.create()
 
 
-@pytest.mark.django_db
-def test_can_create_address_instance():
-    user = User.objects.create()
-    address = Address(user=user, current=False)
-    address.save()
+    @pytest.mark.django_db
+    def test_can_create_address_instance(self):
+        user = User.objects.create()
+        address = Address(user=user, current=False)
+        address.save()
