f58c345f12b650d03b99e0efe7ccf20354d14728
Damian Tabor <zettie008@gmail.com>
2021-09-14 19:18:03 +0100

    Test if can create Address instance

--- tests/address/test_models.py
+++ tests/address/test_models.py
@@ -1,10 +1,17 @@
 import pytest
 
 from django.db import IntegrityError
-from address.models import Address
+from address.models import Address, User
 
 
 @pytest.mark.django_db
 def test_address_model_raises_integrityerror():
     with pytest.raises(IntegrityError):
         Address.objects.create()
+
+
+@pytest.mark.django_db
+def test_can_create_address_instance():
+    user = User.objects.create()
+    address = Address(user=user, current=False)
+    address.save()
