a993e569c93c7c04af028437f997a5481dcde142
Damian Tabor <zettie008@gmail.com>
2021-09-14 19:14:10 +0100

    Check if Address model raises IntegrityError

--- tests/address/test_models.py
+++ tests/address/test_models.py
@@ -5,6 +5,6 @@ from address.models import Address
 
 
 @pytest.mark.django_db
-def test_address_model():
+def test_address_model_raises_integrityerror():
     with pytest.raises(IntegrityError):
         Address.objects.create()
