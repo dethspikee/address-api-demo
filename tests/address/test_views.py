34483f4ba4aa330838f05e8236a65546ad9ed1b0
Damian Tabor <zettie008@gmail.com>
2021-09-14 23:23:53 +0100

    Check if 401 is returned if user is not authenticated

--- tests/address/test_views.py
+++ tests/address/test_views.py
@@ -19,3 +19,14 @@ def test_can_get_all_addresses(add_address):
 
     assert Address.objects.count() == 2
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_addresses_view_returns_401_if_not_logged_in():
+    factory = APIRequestFactory()
+    view = AddressesView.as_view()
+    request = factory.get("/api/v1.0/addresses/")
+    response = view(request)
+    
+    assert response.data["detail"].code == "not_authenticated"
+    assert response.status_code == 401
