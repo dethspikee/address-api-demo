549a0a6e21cd8bb95d4c440ba88f4f56ba3f8ec0
Damian Tabor <zettie008@gmail.com>
2021-09-14 19:28:50 +0100

    Update test

--- tests/address/test_models.py
+++ tests/address/test_models.py
@@ -15,5 +15,19 @@ class TestAddressModel:
     @pytest.mark.django_db
     def test_can_create_address_instance(self):
         user = User.objects.create()
-        address = Address(user=user, current=False)
+        address_info = {
+            "street": "Test street",
+            "postcode": "N123FG",
+            "town": "London",
+            "country": "GBR",
+            "current": False,
+        }
+        address = Address(user=user, **address_info)
         address.save()
+
+        assert address.user == user
+        assert address.street == address_info["street"]
+        assert address.postcode == address_info["postcode"]
+        assert address.town == address_info["town"]
+        assert address.country == address_info["country"]
+        assert address.current == address_info["current"]
