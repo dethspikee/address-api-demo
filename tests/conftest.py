bb2a49b3b9987cea225d20e592d1d6964d8012fd
Damian Tabor <zettie008@gmail.com>
2021-09-15 22:46:41 +0100

    Add new fixture for adding users

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from address.models import Address
+from address.models import Address, User
 
 
 @pytest.fixture(scope="function")
@@ -10,3 +10,11 @@ def add_address():
                 postcode=postcode, town=town, country=country, current=current)
         return address
     return _add_address
+
+
+@pytest.fixture(scope="function")
+def add_user():
+    def _add_user(username):
+        user = User.objects.create(username=username)
+        return user
+    return _add_user
