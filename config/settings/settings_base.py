7e3fb37e721a0eff3424f5a5d07ad11e6ce2e924
Damian Tabor <zettie008@gmail.com>
2021-09-23 14:18:30 +0100

    Move DRF settings to local settings file

--- config/settings/settings_base.py
+++ config/settings/settings_base.py
@@ -145,19 +145,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'address.User'
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,
-    'NON_FIELD_ERRORS_KEY': 'error',
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ],
-}
-
 REST_AUTH_SERIALIZERS = {
     'TOKEN_SERIALIZER': 'address.serializers.MyTokenSerializer',
 }
--- config/settings/settings_local.py
+++ config/settings/settings_local.py
@@ -3,6 +3,20 @@ import secrets
 from .settings_base import *
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+    'NON_FIELD_ERRORS_KEY': 'error',
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
+
+
 SECRET_KEY = secrets.token_urlsafe()
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
