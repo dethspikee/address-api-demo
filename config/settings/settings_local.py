377add379c45ff2b959afb58f3d98eb47a8567bb
Damian Tabor <zettie008@gmail.com>
2021-09-23 14:55:05 +0100

    Disable email verification£

--- config/settings/settings_local.py
+++ config/settings/settings_local.py
@@ -20,6 +20,8 @@ DATABASES = {
     }
 }
 
+ACCOUNT_EMAIL_VERIFICATION = "none"
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
