6d381688d661d957372b97b03222d92454e3206d
Damian Tabor <zettie008@gmail.com>
2021-09-23 13:18:46 +0100

    Change to swagger schema ui

--- config/urls.py
+++ config/urls.py
@@ -34,5 +34,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1.0/', include('address.urls')),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
