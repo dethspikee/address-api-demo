3f131841409df8a7e18c3c87e9d6333d5ddccae2
Damian Tabor <zettie008@gmail.com>
2021-09-27 21:21:14 +0100

    Add whitenoise

--- config/settings/settings_base.py
+++ config/settings/settings_base.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -176,3 +177,6 @@ SWAGGER_SETTINGS = {
 }
 
 SITE_ID = 1
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
