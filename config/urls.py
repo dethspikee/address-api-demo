be03b1359ac09a88f3648bac8f9e168cbaf3a866
Damian Tabor <zettie008@gmail.com>
2021-09-27 22:47:40 +0100

    Redirect from root to docs

--- config/urls.py
+++ config/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -33,4 +35,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1.0/', include('address.urls')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', RedirectView.as_view(pattern_name="schema-swagger-ui")),
 ]
